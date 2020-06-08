c443e1eaddb145706cf077b10bd23c1a0312546f
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2020-06-08 16:54:39 +0200

    feat: add app helper

---
+++ lib/frontman/helpers/app_helper.rb
@@ -0,0 +1,19 @@
+# typed: true
+# frozen_string_literal: false
+
+require 'frontman/config'
+
+module AppHelper
+  extend T::Sig
+
+  sig { returns(T::Boolean) }
+  def build?
+    Frontman::Config.get :mode == 'build'
+  end
+
+  sig { returns(T::Boolean) }
+  def serve?
+    !build?
+  end
+
+end
