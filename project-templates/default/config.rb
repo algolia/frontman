5e9c6f59b62fad6102168a9cc8c90c526ac0cd6f
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2020-07-06 11:03:35 +0200

    feat: add main.erb layout

--- project-templates/default/config.rb
+++ project-templates/default/config.rb
@@ -4,7 +4,9 @@ register_data_dirs(['data'])
 register_helper_dir('helpers')
 
 register_layout 'sitemap.xml', nil
-register_layout '*', 'main.haml'
+register_layout '*', 'main.erb'
+# Or use 'main.haml' if you prefer using HAML:
+# register_layout '*', 'main.haml'
 
 Frontman::Config.set(:domain, 'https://example.com')
 
