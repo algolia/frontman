dfab16b9d79a5b29f29e01a437f1bbf891af0c34
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2020-08-28 11:43:28 +0200

    fix: ensure DataStoreFile does not convert to custom struct

--- spec/frontman/data_store_file_spec.rb
+++ spec/frontman/data_store_file_spec.rb
@@ -3,7 +3,17 @@
 
 require './spec/spec_setup'
 require 'lib/frontman/data_store_file'
+require 'lib/frontman/data_store'
 
 describe Frontman::DataStoreFile do
-  # TODO: determine best tests
+  subject do
+    data = Frontman::DataStore.new("#{__dir__}/mocks")
+    data.info
+  end
+
+  describe '#to_ostruct' do
+    it 'does not convert to custom struct' do
+      expect(subject.to_ostruct.is_a?(Frontman::DataStoreFile)).to eq true
+    end
+  end
 end
